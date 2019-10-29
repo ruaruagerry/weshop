954be6d877910a301ef50e5c4f9768a9bf18196c
GerryLuo <dkluohanga@163.com>
2019-10-29 21:07:37 +0800

    服务器代码删掉

--- handles/goods/goods_detail.go
+++ handles/goods/goods_detail.go
@@ -4,9 +4,11 @@ import (
 	"encoding/json"
 	"weshop/gconst"
 	"weshop/pb"
+	"weshop/rconst"
 	"weshop/server"
 
 	"github.com/golang/protobuf/proto"
+	"github.com/gomodule/redigo/redis"
 )
 
 type detailReq struct {
@@ -14,7 +16,10 @@ type detailReq struct {
 }
 
 type detailRsp struct {
-	
+	Gallery   []string                `json:"gallery"`
+	Attribute []*rconst.GoodAttribute `json:"attribute"`
+	Issue     []*rconst.GoodIssue     `json:"issue"`
+	Brand     rconst.GoodBrand        `json:"brand"`
 }
 
 func detailHandle(c *server.StupidContext) {
@@ -35,33 +40,72 @@ func detailHandle(c *server.StupidContext) {
 	}
 	log.Info("detailHandle enter, req:", string(c.Body))
 
-	// conn := c.RedisConn
+	conn := c.RedisConn
 	// playerid := c.UserID
 
-	// // redis multi get
-	// conn.Send("MULTI")
-	// redisMDArray, err := redis.Values(conn.Do("EXEC"))
-	// if err != nil {
-	// 	httpRsp.Result = proto.Int32(int32(gconst.ErrRedis))
-	// 	httpRsp.Msg = proto.String("统一获取缓存操作失败")
-	// 	log.Errorf("code:%d msg:统一获取缓存操作失败 redisMDArray Values err, err:%s", httpRsp.GetResult(), err.Error())
-	// 	return
-	// }
+	// redis multi get
+	conn.Send("MULTI")
+	conn.Send("HGET", rconst.HashGoodsGallery, req.GoodID)
+	conn.Send("HGET", rconst.HashGoodsAttribute, req.GoodID)
+	conn.Send("HGET", rconst.HashGoodsIssue, req.GoodID)
+	conn.Send("HGET", rconst.HashGoodsBrand, req.GoodID)
+	redisMDArray, err := redis.Values(conn.Do("EXEC"))
+	if err != nil {
+		httpRsp.Result = proto.Int32(int32(gconst.ErrRedis))
+		httpRsp.Msg = proto.String("统一获取缓存操作失败")
+		log.Errorf("code:%d msg:统一获取缓存操作失败 redisMDArray Values err, err:%s", httpRsp.GetResult(), err.Error())
+		return
+	}
+
+	gallerybyte, _ := redis.Bytes(redisMDArray[0], nil)
+	attributebyte, _ := redis.Bytes(redisMDArray[1], nil)
+	issuebyte, _ := redis.Bytes(redisMDArray[2], nil)
+	brandbyte, _ := redis.Bytes(redisMDArray[3], nil)
 
 	// // do something
+	gallery := []string{}
+	err = json.Unmarshal(gallerybyte, &gallery)
+	if err != nil {
+		httpRsp.Result = proto.Int32(int32(gconst.ErrRedis))
+		httpRsp.Msg = proto.String("展览图unmarshal解析失败")
+		log.Errorf("code:%d msg:展览图unmarshal解析失败 gallerybyte Unmarshal err, err:%s", httpRsp.GetResult(), err.Error())
+		return
+	}
 
-	// // redis multi set
-	// conn.Send("MULTI")
-	// _, err = conn.Do("EXEC")
-	// if err != nil {
-	// 	httpRsp.Result = proto.Int32(int32(gconst.ErrRedis))
-	// 	httpRsp.Msg = proto.String("统一存储缓存操作失败")
-	// 	log.Errorf("code:%d msg:统一存储缓存操作失败 exec err, err:%s", httpRsp.GetResult(), err.Error())
-	// 	return
-	// }
+	attribute := []*rconst.GoodAttribute{}
+	err = json.Unmarshal(attributebyte, &attribute)
+	if err != nil {
+		httpRsp.Result = proto.Int32(int32(gconst.ErrRedis))
+		httpRsp.Msg = proto.String("商品参数unmarshal解析失败")
+		log.Errorf("code:%d msg:商品参数unmarshal解析失败 attributebyte Unmarshal err, err:%s", httpRsp.GetResult(), err.Error())
+		return
+	}
+
+	issue := []*rconst.GoodIssue{}
+	err = json.Unmarshal(issuebyte, &issue)
+	if err != nil {
+		httpRsp.Result = proto.Int32(int32(gconst.ErrRedis))
+		httpRsp.Msg = proto.String("商品常见问题unmarshal解析失败")
+		log.Errorf("code:%d msg:商品常见问题unmarshal解析失败 issuebyte Unmarshal err, err:%s", httpRsp.GetResult(), err.Error())
+		return
+	}
+
+	brand := &rconst.GoodBrand{}
+	err = json.Unmarshal(brandbyte, &brand)
+	if err != nil {
+		httpRsp.Result = proto.Int32(int32(gconst.ErrRedis))
+		httpRsp.Msg = proto.String("商品牌子unmarshal解析失败")
+		log.Errorf("code:%d msg:商品牌子unmarshal解析失败 brandbyte Unmarshal err, err:%s", httpRsp.GetResult(), err.Error())
+		return
+	}
 
 	// rsp
-	rsp := &detailRsp{}
+	rsp := &detailRsp{
+		Gallery:   gallery,
+		Attribute: attribute,
+		Issue:     issue,
+		Brand:     brand,
+	}
 	data, err := json.Marshal(rsp)
 	if err != nil {
 		httpRsp.Result = proto.Int32(int32(gconst.ErrParse))
--- rconst/goods_table.go
+++ rconst/goods_table.go
@@ -9,7 +9,34 @@ type Good struct {
 	Price    int64  `json:"price"`
 }
 
+// GoodAttribute 商品参数
+type GoodAttribute struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+// GoodIssue 商品常见问题
+type GoodIssue struct {
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+}
+
+// GoodBrand 商品商标
+type GoodBrand struct {
+	BrandID   string `json:"brandid"`
+	BrandName string `json:"brandname"`
+}
+
 const (
-	// HashGoodsInfoPrefix 商品列表
+	// HashGoodsInfoPrefix 商品列表+categoryid
 	HashGoodsInfoPrefix = "weshop:goods:info:"
+
+	// HashGoodsGallery 商品展览图
+	HashGoodsGallery = "weshop:goods:gallery"
+	// HashGoodsAttribute 商品属性
+	HashGoodsAttribute = "weshop:goods:attribute"
+	// HashGoodsIssue 商品常见问题
+	HashGoodsIssue = "weshop:goods:issue"
+	// HashGoodsBrand 商品牌子
+	HashGoodsBrand = "weshop:goods:brand"
 )
